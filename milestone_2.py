babedc3e63224ae91cf25f75deb73b616d94a946
Dule Gyurkov <dulegyurkov@Dules-Air.lan>
2024-08-26 20:57:10 +0100

    Upated with code and READme file

---
+++ milestone_2.py
@@ -0,0 +1,13 @@
+import random
+word_list = ['apple', 'banana', 'peach', 'pineapple', 'strawberry']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input('Please enter your choice!')
+if len(guess) <= 1 and guess.isalpha():
+      print('Good guess!')
+else:
+      print('Oops that is not a valid input')
+    
