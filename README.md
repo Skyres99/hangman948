862728eb533ca3bd640c11d7cb562a9862ce1d1c
Dule Gyurkov <dulegyurkov@Dules-Air.lan>
2024-09-11 01:47:25 +0100

    Last milestone of the project completed

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ Hangman is a classic game in which a player thinks of a word and the other playe
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
 Hangman Game is my first Python project that helps me implement my knoweldge into a hands on experience and opening the path of many more and complex programming projects.
-
+Moreover Hangman Game is created by using only Python programming language.
 
 Hangman Game is developed in a way that computer chooses a random word of list of words and the user needs to guess a letter of that word.
 Starting by default of 5 lives or right of 5 mistakes, furthermore more than 5 wrong guesses, the game is over and the player losses.
+
+## Installation
+In order to install the game, first you need to ensure that you have installed Git and Python latest version in your machine.
+1. Clone the repository:
+git clone https://.github.com/Skyres99/hangman948.git
+2. Navigate into directory following the path regards to the location of the repository.
+cd ~/hangman948
+cd Desktop/hangman948
+cd Downloads/hangman948
---
+++ milestone_5.py
@@ -0,0 +1,54 @@
+import random
+
+word_list = ['apple', 'banana', 'peach', 'pineaaple', 'strawberry']
+
+class Hangman():
+           
+      def __init__(self, word_list, num_lives = 5):
+            self.word = random.choice(word_list)
+            self.word_guessed = ['_', '_', '_', '_', '_'] * len(self.word)
+            self.num_letters = len(set(self.word))
+            self.num_lives = num_lives
+            self.word_list = word_list
+            self.list_of_guesses = []
+            
+      def check_guess(self, guess):
+            if guess in self.word:
+                  print(f'Good guess! {guess} is in the word')
+                  for _ in range(len(self.word)):
+                        if self.word[_] == guess:
+                              self.word_guessed[_] == guess
+                  self.num_letters -= 1
+            else:
+                  self.num_lives -= 1
+                  print(f'Sorry, {guess} is not in the word.')
+                  print(f'You have {self.num_lives} lives left')
+            
+      def ask_for_input(self):
+            while True:
+                  self.guess = input('Please enter your character.')
+                  self.guess = self.guess.lower()
+                  if len(self.guess) != 1 and not self.guess.isalpha():
+                        print('Invalid letter. Please, enter a single alphabetical character.')
+                  elif self.guess in self.list_of_guesses:
+                        print('You alredy tried that letter!')
+                  else:
+                        self.list_of_guesses.append(self.guess)
+                        self.check_guess(self.guess)
+                        break
+
+def play_game(word_list):
+      num_lives = 5 
+      game = Hangman(word_list, num_lives)
+
+      while True:
+            if game.num_lives <= 0:
+                  print('You lost!')
+                  break
+            elif game.num_letters == 0:
+                  print('Congratulations. You won!')
+                  break
+            else:
+                  game.ask_for_input()
+                  
+play_game(word_list)
